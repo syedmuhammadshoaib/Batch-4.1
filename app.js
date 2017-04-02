fd4fe05924d0b3e4562cdb0785f5a9d1bc4e29a0
syedmuhammadshoaib <s.m.shoaib@hotmail.com>
2017-04-02 23:36:39 +0500

    Theory Class Assignments
    Batch 4.1 js

--- app.js
+++ app.js
@@ -1,7 +1,75 @@
+/* Assignment 1*/
+
+var table;
+var start=0;
+var end;
 
 
-var hello;
+table =+prompt("Enter Table no","2");
 
-hello="Hello World";
+start =+prompt("Enter starting value","1");
 
-alert(hello+"!"+"Rollno:4730 syed Muhammad shoaib");
+end =+prompt("Enter ending Value","5");
+document.write("<h2>Assignment 1 </h2><br/>");
+for(i=start;i<=end; i++)
+{
+    document.write(table+"*"+i+"=",table*i+"<br/> ");
+   
+}
+
+ document.write("<br/>"); document.write("<br/>");
+/* Assignment 2*/
+
+document.write("<h2>Assignment 2 </h2><br/>");
+for(var i=1;i<=8; i++)
+{
+       
+for(var j=0;j<i; j++)
+{
+
+    document.write("*");
+  
+}  
+ document.write("<br/>");
+}
+ document.write("<br/>"); document.write("<br/>");
+
+/* Assignment 3*/
+
+document.write("<h2>Assignment 3</h2><br/>");
+var end=6;
+for(var i=1;i<=end; i++)
+{
+//document.write(" ");   
+
+for(var j=i;j < end; j++)
+{
+  document.write("#");
+}
+for(var k=1;k <=(2*i-1) ; k++)
+{
+  document.write("*");
+}
+document.write("</br>");
+
+//end--;  
+}
+
+for(var i=end;i >=1; i--)
+{
+//document.write(" ");   
+
+for(var j=i;j < end; j++)
+{
+  document.write("#");
+}
+for(var k=1;k <=(2*i-1) ; k++)
+{
+  document.write("*");
+}
+document.write("</br>");
+
+//end--;  
+}
+
+
