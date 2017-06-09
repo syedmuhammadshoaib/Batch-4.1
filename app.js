c666158e04d32172f2c35725fce916e6360a719e
syedmuhammadshoaib <s.m.shoaib@hotmail.com>
2017-06-10 02:15:58 +0500

    ToDoApp

--- app.js
+++ app.js
@@ -1,75 +1,47 @@
-/* Assignment 1*/
 
-var table;
-var start=0;
-var end;
+$(function(){$('#error').hide();})
 
-
-table =+prompt("Enter Table no","2");
-
-start =+prompt("Enter starting value","1");
-
-end =+prompt("Enter ending Value","5");
-document.write("<h2>Assignment 1 </h2><br/>");
-for(i=start;i<=end; i++)
+function todoFunctionForInput()
 {
-    document.write(table+"*"+i+"=",table*i+"<br/> ");
-   
-}
+ 
+var check=$('#todo').val();
+    if( check!="")
+    {      
+     $('#error').hide();
 
- document.write("<br/>"); document.write("<br/>");
-/* Assignment 2*/
+var createTag=document.createElement('li');
+var textTag=document.createTextNode(check);
 
-document.write("<h2>Assignment 2 </h2><br/>");
-for(var i=1;i<=8; i++)
-{
-       
-for(var j=0;j<i; j++)
-{
+var image=document.createElement('img');
+image.src="Images/single.png";
 
-    document.write("*");
+image.style.cssFloat="";
+image.onclick=function(){
+var li=this.parentNode;
+var ol=li.parentNode;
+ol.removeChild(li);
   
-}  
- document.write("<br/>");
 }
- document.write("<br/>"); document.write("<br/>");
+createTag.appendChild(textTag);
+createTag.appendChild(image);
+document.getElementById('addlist').appendChild(createTag);
+document.getElementById('todo').value=''; 
+}  else {
+$('#error').show();
+        // document.getElementById("error").innerHTML='Please add your work to do!';
+    
+    }
 
-/* Assignment 3*/
+  }
 
-document.write("<h2>Assignment 3</h2><br/>");
-var end=6;
-for(var i=1;i<=end; i++)
+    
+
+  
+
+
+function removeall()
 {
-//document.write(" ");   
-
-for(var j=i;j < end; j++)
-{
-  document.write("#");
+alert("Do you really want to delete.");
+document.getElementById('addlist').innerHTML="";
 }
-for(var k=1;k <=(2*i-1) ; k++)
-{
-  document.write("*");
-}
-document.write("</br>");
-
-//end--;  
-}
-
-for(var i=end;i >=1; i--)
-{
-//document.write(" ");   
-
-for(var j=i;j < end; j++)
-{
-  document.write("#");
-}
-for(var k=1;k <=(2*i-1) ; k++)
-{
-  document.write("*");
-}
-document.write("</br>");
-
-//end--;  
-}
-
 
